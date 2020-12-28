205f2ebe8e7c924137b6562c594ebf16ec0e8710
Phong <phong1233@gmail.com>
2020-12-27 23:02:44 -0500

    add reference card

--- src/modules/HomePage.jsx
+++ src/modules/HomePage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -12,6 +16,20 @@ const useStyles = makeStyles({
     paddingRight: '20px',
     paddingLeft: '20px',
     textAlign: 'center'
+  },
+  cardContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '30px'
+  },
+  card: {
+    width: '400px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '30px'
   }
 });
 
@@ -35,6 +53,22 @@ export default function HomePage() {
       <Typography className={classes.title} variant='body1'>
         You can also shared this list with anyone      
       </Typography>
+      <div className={classes.cardContainer}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography>
+              <Link href="https://github.com/phong1233" color="secondary">
+                By: Phong Le
+              </Link>
+            </Typography>
+            <Typography>
+              <Link href="https://github.com/phong1233/shoppies" color="secondary">
+                Source code
+              </Link>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
